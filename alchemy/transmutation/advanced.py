8fc11c03c3138b8460ad6f7639368b94c744dde3
maryvi <maryvi.jobfinder@gmail.com>
2026-03-16 15:07:02 +0000

    ex3

---
+++ alchemy/transmutation/advanced.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python3
+
+
+from ..potions import healing_potion
+from .basic import lead_to_gold
+
+
+def philosophers_stone() -> str:
+    return ( 
+        "Philosopher's stone created "
+        f"using {lead_to_gold()} and {healing_potion()}"
+    )
+
+def elixir_of_life() -> str:
+    return "Elixir of life: eternal youth achieved!"
