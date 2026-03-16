561aab6bc5938628a90a2d23d28390da66ce4d63
maryvi <maryvi.jobfinder@gmail.com>
2026-03-16 11:33:36 +0000

    ex2

---
+++ alchemy/potions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+from alchemy.elements import (
+    create_fire,
+    create_water,
+    create_earth,
+    create_air
+)
+
+
+def healing_potion() -> str:
+    fire: str = create_fire()
+    water: str = create_water()
+    return f"Healing potion brewed with {fire} and {water}"
+
+
+def strength_potion() -> str:
+    fire: str = create_fire()
+    earth: str = create_earth()
+    return f"Strength potion brewed with {earth} and {fire}"
+
+
+def invisibility_potion() -> str:
+    air: str = create_air()
+    water: str = create_water()
+    return f"Invisibility potion brewed with {air} and {water}"
+
+
+def wisdom_potion() -> str:
+    fire: str = create_fire()
+    earth: str = create_earth()
+    air: str = create_air()
+    water: str = create_water()
+    all: str = f"{fire} {water} {earth} {air}"
+    return f"Wisdom potion brewed with {all}"
---
+++ ft_import_transmutation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import alchemy.elements
+
+from alchemy import create_water
+from alchemy.elements import create_fire, create_earth
+from alchemy.potions import strength_potion
+
+from alchemy.potions import healing_potion as heal
+
+
+def main() -> None:
+    print("\n=== Import Transmutation Mastery ===")
+    print("\nMethod 1 - Full module import")
+    print(f"alchemy.elements.create_fire(): {alchemy.elements.create_fire()}")
+    print("\nMethod 2 - Specific function import")
+    print(f"create_water(): {create_water()}")  # importa desde el __init__
+    print("\nMethod 3 - Aliased import")
+    print(f"heal(): {heal()}")
+    print("\nMethod 4 - Multiple imports")
+    print(f"create_earth(): {create_earth()}")
+    print(f"create_fire(): {create_fire()}")
+    print(f"strength_potion(): {strength_potion()}")
+
+    print("\nAll import transmutation methods mastered!")
+
+
+if __name__ == "__main__":
+    main()
